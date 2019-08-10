82e681fa062d7e9b4bf049c198aa820177551dbf
liang <zh_l2006@sina.com>
2019-08-09 20:37:56 -0600

    placeholder for optract client

---
+++ extension/UI/src/service/OptractClient.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import IPFS from 'ipfs';
+import ipfsClient from 'ipfs-http-client';
+
+
+
+class OptractClient {
+    constructor() {
+    
+    }
+
+}
+
+const optractClient = new OptractClient();
+export default optractClient;
