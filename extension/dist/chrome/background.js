6b45ee92c2770ec466db6013853e8661876ac351
jasonlin <jasonlin@elevenbuckets.com>
2019-09-10 13:50:56 -0600

    check in modified scripts got from python-script branch and making browser connection to work. will work with @liangzl on daemon close

--- extension/dist/chrome/background.js
+++ extension/dist/chrome/background.js
@@ -1,29 +1,40 @@
-function openTab(filename) { var myid = chrome.i18n.getMessage("@@extension_id"); chrome.windows.getCurrent( function(win) { chrome.tabs.query({'windowId': win.id}, function(tabArray) { for(var i in tabArray) { if(tabArray[i].url == "chrome-extension://" + myid + "/" + filename) { // console.log("already opened");
- chrome.tabs.update(tabArray[i].id, {active: true}); return; } } chrome.tabs.create({url:chrome.extension.getURL(filename)}); }); }); }
+function openTab(filename) {
+	var myid = chrome.i18n.getMessage("@@extension_id"); chrome.windows.getCurrent(function (win) {
+		chrome.tabs.query({ 'windowId': win.id }, function (tabArray) {
+			for (var i in tabArray) {
+				if (tabArray[i].url == "chrome-extension://" + myid + "/" + filename) { // console.log("already opened");
+					chrome.tabs.update(tabArray[i].id, { active: true }); return;
+				}
+			} chrome.tabs.create({ url: chrome.extension.getURL(filename) });
+		});
+	});
+}
 
 openTab("index.html")
 
 function isNewTab(tab, url) {
 	return (
-	  typeof url === 'undefined' && tab.active && tab.url === 'chrome://newtab/'
+		typeof url === 'undefined' && tab.active && tab.url === 'chrome://newtab/'
 	)
-  }
-chrome.browserAction.onClicked.addListener(function(activeTab, url)
-{
-	if(isNewTab(activeTab, url) ){
+}
+chrome.browserAction.onClicked.addListener(function (activeTab, url) {
+	if (isNewTab(activeTab, url)) {
 		openTab("index.html")
 	}
-   
+
 });
 
-chrome.runtime.onConnect.addListener(function(port) {
-    port.onMessage.addListener(function(msg) {
-	//tport.disconnect();
-	let tport = chrome.runtime.connectNative('optract');
-	tport.onMessage.addListener(function (msgs) {
- 		true;
-	})
-    });
+var tport = chrome.runtime.connectNative('optract');
+tport.onMessage.addListener(function (msgs) {
+	console.log(msgs);
+})
 
-    port.onDisconnect.addListener(function(){ tport.disconnect(); })
+chrome.runtime.onConnect.addListener(function (port) {
+	port.onMessage.addListener(function (msg) {
+		// Need to put nativeApp.py under dist directory, and update the optract.json under ~/.config/google-chrome/NativeMessagingHosts 
+		// to use nativeApp.py
+		tport.postMessage({ text: "ping" });
+	});
+
+	port.onDisconnect.addListener(function () { tport.disconnect(); })
 });
