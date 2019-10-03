22bc34b92b4d02554dbe402e0748ceb992dc2259
jasonlin <jasonlin@elevenbuckets.com>
2019-10-03 17:25:44 -0600

    passing selected text back to content script using window.postMessage as suggested in official doc

--- extension/dist/chrome/content.js
+++ extension/dist/chrome/content.js
@@ -4,13 +4,30 @@ chrome.runtime.sendMessage({landing: true},
     console.log(`DEBUG: got response from background`)
     if (response.yourParent.includes('chrome-extension://')) {
     	    console.log(`DEBUG: This is an Optract recommended page!`);
+
+	    //var txt = document.createElement("textarea"); 
+	    //txt.setAttribute('id', 'OptractSelection');
+	    //var sty = document.createElement("style"); 
+	    //styles = '#OptractSelection { width: 300px; height: 120px; pointer-events: none; position: fixed; top: 5px; right: 10px; display: none; } ';
+	    //sty.appendChild(document.createTextNode(styles));
+
+            //document.body.appendChild(txt);
+            //document.body.appendChild(sty);
+	    
+	    window.addEventListener('message', function (e) {
+		    if (e.source != window) return;
+		    if (e.data.type === 'OPTRACT_QUOTE') {
+			    console.log(`DEBUG: ContentScript get quote:`);
+			    console.dir(e.data);
+		    }
+	    })
+
 	    var actualCode = 
-	        'var txt = document.createElement("textarea"); '
-	      + 'txt.style.width = "300px"; '
-	      + 'txt.style.height = "120px"; '
-	      + 'txt.style.zIndex = "100"; '
+		'var OptractSelectTimer; '
 	      + 'function selectionCheck (e) { setTimeout(getSelected, 180) }; '
-	      + 'function getSelected () { txt.value = window.getSelection().toString(); console.log(`DEBUG: selection is ${txt.value}`); }'
+	      + 'function getSelected () { let txt = window.getSelection().toString(); '
+	      +                          ' clearTimeout(OptractSelectTimer); ' 
+	      +                          ' if (txt) OptractSelectTimer = setTimeout(window.postMessage, 1200, {type: "OPTRACT_QUOTE", txt}); }'
 	      + 'document.addEventListener("selectionchange", selectionCheck); ';
 	    var script = document.createElement('script');
             script.textContent = actualCode;
