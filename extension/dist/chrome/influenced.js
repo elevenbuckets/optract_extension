96a43cc94c41a5b2c49c3d8ba804fb01a7d47f1f
jasonlin <jasonlin@elevenbuckets.com>
2019-10-13 21:40:58 -0600

    close popup automatically after 5 secs

--- extension/dist/chrome/influenced.js
+++ extension/dist/chrome/influenced.js
@@ -5,6 +5,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
 	     let domain = activeTab.url.split('/')[2];
 	     if (response.result === true && !document.getElementById('content').innerText.includes(domain)) {
 		     document.getElementById('content').innerHTML = '<h2>' + domain + '</h2>';
+		     let myTimer = setTimeout(function() { window.close(); }, 5000)
 	     } else {
 		     console.log(`DEBUG: error during popup send influence`)
 	     }
